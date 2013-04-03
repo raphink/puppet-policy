fa7892fad5b1b7ef628b6e0486f7c06c67af0cba
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-03 09:11:02 +0200

    Fix exechelper to really fail when it should

--- lib/spec/exechelper.rb
+++ lib/spec/exechelper.rb
@@ -1,8 +1,9 @@
 module PuppetSpec
   module ExecHelper
     def ssh_exec(cmd, opts={})
-      stdin, stdout, stderr = Open3.popen3(cmd)
-      { :stdout => stdout.gets, :stderr => stderr.gets, :exit_code => $?.to_i, :exit_signal => nil }
+      stdout, stderr, status = Open3.capture3(cmd)
+      { :stdout => stdout, :stderr => stderr,
+        :exit_code => status, :exit_signal => nil }
     end
   end
 end
