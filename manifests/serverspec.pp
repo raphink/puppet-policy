b992ac39255198a7d0a199b40fd5c523c2d7f8df
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-08 22:43:09 +0200

    Add doc for spec::serverspec

--- manifests/serverspec.pp
+++ manifests/serverspec.pp
@@ -1,3 +1,30 @@
+# Definition: spec::serverspec
+#
+# Deploy an rspec file to :vardir/spec/classes/
+#
+# Parameters:
+#   ['ensure']         - Whether the test should be present or absent
+#   ['filename']       - The file name for the test, optional
+#   ['classname']      - The class name the test should be associated with,
+#                        defaults to $fqdn
+#   ['content']        - The content of the test file (rspec code)
+#   ['source']         - Source of the test file (unless content is specified)
+#
+# Actions:
+# - Deploys an serverspec test file to :vardir/spec/class
+#
+# Sample Usage:
+#
+#   spec::serverspec { 'check ssh service':
+#     ensure    => present,
+#     classname => 'ssh',
+#     content   => 'describe "ssh" do
+#     it { should be_installed }
+#     it { should be_running }
+#     it { should be_enabled }
+#   end',
+#   }
+#
 define spec::serverspec (
   $ensure = 'present',
   $filename = '',
