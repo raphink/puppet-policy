dddc19e163004fd0452f3ec3bbbfd35f31375844
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-23 12:17:31 +0200

    Fix typo in Group plugin

--- lib/puppetx/policy/auto_spec/group.rb
+++ lib/puppetx/policy/auto_spec/group.rb
@@ -1,6 +1,6 @@
 Puppetx::Policy::AutoSpec.newspec 'Group' do |g|
   content = "  describe group('#{g[:name]}') do\n"
-  if u[:ensure] == 'present'
+  if g[:ensure] == 'present'
     content += "    it { should exist }\n"
     content += "    it { should have_gid('#{g[:gid]}') }\n" if g[:gid]
   else
