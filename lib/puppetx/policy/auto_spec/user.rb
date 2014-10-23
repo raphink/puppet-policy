ad9bfe30f73b261e515b45f31f61baf78f7a6781
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-23 10:35:03 +0200

    Fix User plugin when user is absent

--- lib/puppetx/policy/auto_spec/user.rb
+++ lib/puppetx/policy/auto_spec/user.rb
@@ -1,14 +1,16 @@
 Puppetx::Policy::AutoSpec.newspec 'User' do |u|
   content = "  describe user('#{u[:name]}') do\n"
-  content += "    it { should exist }\n" if u[:ensure] == 'present'
-  content += "    it { should_not exist }\n" if u[:ensure] == 'absent'
-  content += "    it { should have_uid('#{u[:uid]}') }\n" if u[:uid]
-  content += "    it { should belong_to_group('#{u[:gid]}') }\n" if u[:gid]
-  (u[:groups] || []).any? do |g|
-    content += "    it { should belong_to_group('#{g}') }\n"
+  if u[:ensure] == 'present'
+    content += "    it { should exist }\n"
+    content += "    it { should have_uid('#{u[:uid]}') }\n" if u[:uid]
+    content += "    it { should belong_to_group('#{u[:gid]}') }\n" if u[:gid]
+    (u[:groups] || []).any? do |g|
+      content += "    it { should belong_to_group('#{g}') }\n"
+    end
+    content += "    it { should have_home_directory('#{u[:home]}') }\n" if u[:home]
+    content += "    it { should have_login_shell('#{u[:shell]}') }\n" if u[:shell]
+  else
+    content += "    it { should_not exist }\n"
   end
-  content += "    it { should have_home_directory('#{u[:home]}') }\n" if u[:home]
-  content += "    it { should have_login_shell('#{u[:shell]}') }\n" if u[:shell]
   content += "  end\n"
-  content
 end
