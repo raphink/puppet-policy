13ebe816cc3dfe989c92757706aacaa2fd734104
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-23 10:47:01 +0200

    Allow several auto_spec plugins for the same resource type

--- lib/puppetx/policy/auto_spec.rb
+++ lib/puppetx/policy/auto_spec.rb
@@ -1,19 +1,19 @@
 module Puppetx::Policy::AutoSpec
   def self.newspec(type, &block)
-    @auto_specs ||= {}
-    @auto_specs[type] = block
+    @auto_specs ||= []
+    @auto_specs << { :type => type, :block => block }
   end
 
   def self.gen_auto_spec_files(resources, spec_dir)
-    @auto_specs.each do |type, block|
-      spec_content = "describe '#{type} resources' do\n"
+    @auto_specs.each do |spec|
+      spec_content = "describe '#{spec[:type]} resources' do\n"
       spec_content += resources.map do |r|
-        next unless r.type == type
-        block.call(r.to_hash)
+        next unless r.type == spec[:type]
+        spec[:block].call(r.to_hash)
       end.compact.join("\n")
       spec_content += "end\n"
 
-      file_name = type.downcase.gsub(':', '_') + '_spec.rb'
+      file_name = spec[:type].downcase.gsub(':', '_') + '_spec.rb'
       file = File.join(spec_dir, file_name)
 
       File.open(file, 'w') do |f|
