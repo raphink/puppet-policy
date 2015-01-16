f671fd725d022a30aa7b91e611edae9784c69bae
Artur Gadelshin <a_gadelshin@wargaming.net>
2015-01-16 19:34:58 +0300

    enable gem and pip providers for package auto_spec

--- lib/puppetx/policy/auto_spec/package.rb
+++ lib/puppetx/policy/auto_spec/package.rb
@@ -1,4 +1,9 @@
 Puppetx::Policy::AutoSpec.newspec 'Package' do |p|
+  provider = ""
+  providers = ['gem', 'pip']
   should = (p[:ensure] == 'absent' or p[:ensure] == 'purged') ? 'should_not' : 'should'
-  "  describe package('#{p[:name]}') do\n    it { #{should} be_installed }\n  end\n"
+  if providers.include? p[:provider]
+    provider = ".by('#{p[:provider]}')"
+  end
+  "  describe package('#{p[:name]}') do\n    it { #{should} be_installed#{provider} }\n  end\n"
 end
