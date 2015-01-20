04fb6b9998e7c0103c06faeb2f31d427c8a165b9
Artur Gadelshin <a_gadelshin@wargaming.net>
2015-01-20 21:51:23 +0300

    change if-else statement to one-liner for package provider

--- lib/puppetx/policy/auto_spec/package.rb
+++ lib/puppetx/policy/auto_spec/package.rb
@@ -1,9 +1,6 @@
 Puppetx::Policy::AutoSpec.newspec 'Package' do |p|
-  provider = ""
   providers = ['gem', 'pip']
   should = (p[:ensure] == 'absent' or p[:ensure] == 'purged') ? 'should_not' : 'should'
-  if providers.include? p[:provider]
-    provider = ".by('#{p[:provider]}')"
-  end
+  provider = providers.include? p[:provider] ? ".by('#{p[:provider]}')" : ''
   "  describe package('#{p[:name]}') do\n    it { #{should} be_installed#{provider} }\n  end\n"
 end
