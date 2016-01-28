a4ed42beb7b066b94ec41807b899caa564d35170
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-01-28 11:52:31 +0100

    Fix provider test in package auto_spec

--- lib/puppetx/policy/auto_spec/package.rb
+++ lib/puppetx/policy/auto_spec/package.rb
@@ -1,6 +1,6 @@
 Puppetx::Policy::AutoSpec.newspec 'Package' do |p|
   providers = ['gem', 'pip']
   should = (p[:ensure] == 'absent' or p[:ensure] == 'purged') ? 'should_not' : 'should'
-  provider = providers.include? p[:provider] ? ".by('#{p[:provider]}')" : ''
+  provider = providers.include?(p[:provider]) ? ".by('#{p[:provider]}')" : ''
   "  describe package('#{p[:name]}') do\n    it { #{should} be_installed#{provider} }\n  end\n"
 end
